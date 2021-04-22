e88d8d45ac65fa75fba3b6fdfc54d46a37929afb
yutomorita416 <yutomorita416@gmail.com>
2021-04-22 03:05:26 +0000

    finish 4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '6133d15b235be020a25da83e0bb27ae4f1aafdcedb50eb59cdad801db684ab75dab70df7c2c8e2c7abd40c9f4d64ec4d9f61a52cfbf28437ab20d0459011726b'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
   devise_for :users
   root to: "homes#top"
   

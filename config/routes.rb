1aa6575190648bb230c10d6413bf20c9298e51ec
yutomorita416 <yutomorita416@gmail.com>
2021-04-22 05:27:34 +0000

    finish 4-4

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
   devise_for :users
   root to: "homes#top"
   
